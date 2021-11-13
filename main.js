977c4cdcf2e4719993cd6808872a9495e38dd41d
Mega-Charizard <81663277+Mega-Charizard@users.noreply.github.com>
2021-11-12 16:16:57 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function startClassifiction()
+{
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/PTe16rDjE/model.json', modelReady);
+}
+
+function modelReady()
+{
+    classifier.classify(gotResults);
+}
+
+function gotResults(error,results)
+{
+    console.log("gotResults");
+}
